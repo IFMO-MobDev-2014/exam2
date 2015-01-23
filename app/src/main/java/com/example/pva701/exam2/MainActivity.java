466f3351ad70bb7cb90d936032a5cb463256d1bc
Ilya Peresadin <pva701@gmail.com>
2015-01-23 09:19:20 +0300

    init project

--- app/src/main/java/com/example/pva701/exam2/MainActivity.java
+++ app/src/main/java/com/example/pva701/exam2/MainActivity.java
@@ -1,12 +1,12 @@
-package ru.ifmo.md.exam1;
+package com.example.pva701.exam2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
