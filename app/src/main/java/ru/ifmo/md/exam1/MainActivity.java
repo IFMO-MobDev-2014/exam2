340618a4f6b0c37e433dcaedcb5e7878e585c1c4
Dmitry Tomp <dmitrytomp@gmail.com>
2015-01-23 10:44:12 +0000

    Ready, steady...

--- app/src/main/java/ru/ifmo/md/exam1/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/exam1/MainActivity.java
@@ -1,12 +1,12 @@
 package ru.ifmo.md.exam1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +28,6 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
